de6e0861e1ef67fc25efaf751d47342dd321a748
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-08 12:44:03 -0400

    assigns classNames to components and adjusts css placement

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
+import NavBar from './Components/NavBar';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Skills from './Components/Skills';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -13,16 +15,18 @@ function App() {
           <img src={require('./giselle.png')} alt="logo" className="logo" />
         </Link>
       </div>
-      <div className="nav-bar">
-        <Link to="/about"> About </Link>{" "}
-        <Link to="/projects"> Projects </Link>{" "}
-        <Link to="/skills"> Skills </Link>{" "}
-
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route path="/skills" component={Skills} />
-        </Switch>
+      <div className="nav-container">
+        <NavBar />
+      </div>
+      <div className="content-container">
+      <Switch>
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/projects" component={Projects} />
+                    <Route exact path="/skills" component={Skills} />
+                </Switch>
+      </div>
+      <div className="footer-container">
+        <Footer/>
       </div>
     </div>
   );
