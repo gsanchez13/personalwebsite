2ba6dfbb194f6f4abf87117c0be4ce26f60bac8b
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-09 19:33:41 -0400

    adds intro to landing page

---
+++ client/src/Components/Landing.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="landing-div">
+                Welcome. 
+                <br/>
+                My name is Giselle Sanchez, a software developer based in NYC.
+            </div>
+        )
+    }
+}
+export default Landing;
