931dc8fb59e223d28b4551a938491568c6486ef7
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-09 19:19:27 -0400

    adds soccerball animation on about component

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { GiSoccerBall } from 'react-icons/gi';
 
 class About extends Component {
     render() {
         return (
             <div className="about-div">
-                About
+                <span className="gif-container">
+                <GiSoccerBall className="soccer-ball"/>
+                </span>
             </div>
         )
     }
