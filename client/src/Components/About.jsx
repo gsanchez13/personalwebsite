a1c1750286bd6b6cca34c49822a3e4eeff4d143f
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-09 19:34:20 -0400

    styling changes to soccer animation

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -4,7 +4,10 @@ import { GiSoccerBall } from 'react-icons/gi';
 class About extends Component {
     render() {
         return (
-            <div className="about-div">
+            <div className="about-div"> 
+                <br/>
+                <br/>
+                <br/>
                 <span className="gif-container">
                 <GiSoccerBall className="soccer-ball"/>
                 </span>
