8a98577cb9ecedb020b7d2d046ac705f88a35c99
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-10 19:38:20 -0400

    adds a small blurb to about

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -4,13 +4,19 @@ import { GiSoccerBall } from 'react-icons/gi';
 class About extends Component {
     render() {
         return (
-            <div className="about-div"> 
-                <br/>
-                <br/>
-                <br/>
-                <span className="gif-container">
-                <GiSoccerBall className="soccer-ball"/>
-                </span>
+            <div className="about-div">
+                <div className="about-blurb">
+                    I am a born and raised Brooklyner who loves soccer, biking, swimming, dogs, food and all the sunshine I can get.
+                <br />I write code and make cool things happen. Reach out!
+            </div>
+                <br />
+                <br />
+                <br />
+                <div className="route">
+                    <span className="gif-container">
+                        <GiSoccerBall className="soccer-ball" />
+                    </span>
+                </div>
             </div>
         )
     }
