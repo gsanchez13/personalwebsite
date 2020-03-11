36595709856c54f2cfd0c8a5ef8ebae8cdeb251e
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-10 20:41:40 -0400

    changes page name in html

--- client/src/App.js
+++ client/src/App.js
@@ -7,31 +7,33 @@ import About from './Components/About';
 import Projects from './Components/Projects';
 import Skills from './Components/Skills';
 import Footer from './Components/Footer';
+import SwitchBox from'./Components/Switch';
 
 function App() {
   return (
     <div className="App">
-      <div className="logo-container">
-        <Link to="/">
-          <img src={require('./giselle.png')} alt="logo" className="logo" />
-        </Link>
-      </div>
-      <div className="nav-container">
-        <NavBar />
-      </div>
-      <div className="content-container">
-      <Switch>  
-                    <Route exact path="/" component={Landing}/>
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route exact path="/skills" component={Skills} />
-                </Switch>
-      </div>
-      <div className="footer-container">
-        <Footer/>
-      </div>
+      <SwitchBox/>
+        <div className="logo-container">
+          <Link to="/">
+            <img src={require('./giselle.png')} alt="logo" className="logo" />
+          </Link>
+        </div>
+        <div className="nav-container">
+          <NavBar />
+        </div>
+        <div className="content-container">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/skills" component={Skills} />
+          </Switch>
+        </div>
+        <div className="footer-container">
+          <Footer />
+        </div>
     </div>
-  );
-}
-
-export default App;
+      );
+    }
+    
+    export default App;
