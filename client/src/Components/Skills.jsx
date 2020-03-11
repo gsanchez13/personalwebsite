56635dfb6434d0cdcecc159cdd0ae23dfc6534a8
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-10 20:35:15 -0400

    adds new icons to skills section

--- client/src/Components/Skills.jsx
+++ client/src/Components/Skills.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { FaReact, FaNodeJs, FaGitAlt } from 'react-icons/fa';
+import { DiPostgresql, DiJavascript1 } from 'react-icons/di';
+import { TiHtml5 } from 'react-icons/ti';
 
 class Skills extends Component {
     render() {
         return (
             <div className="skills-div">
-                Skills
+                <FaReact />
+                <FaNodeJs />
+                <DiPostgresql/>
+                <DiJavascript1/>
+                <TiHtml5/>
+                <FaGitAlt/>
             </div>
         )
     }
