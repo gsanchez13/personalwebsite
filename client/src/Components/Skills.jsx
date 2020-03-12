c148f62870408efbd44ccde167d63edd738e6f27
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-11 20:35:42 -0400

    creates cards for skills and displays color

--- client/src/Components/Skills.jsx
+++ client/src/Components/Skills.jsx
@@ -5,14 +5,51 @@ import { TiHtml5 } from 'react-icons/ti';
 
 class Skills extends Component {
     render() {
+        const iconArr = [
+            {
+                icon: FaReact,
+                color: "rgb(104, 241, 241)",
+                caption: "React",
+                class: "iconClass"
+            },
+            {
+                icon: FaNodeJs,
+                color: "darkblue",
+                caption: "NodeJS",
+                class: "iconClass"
+            }, {
+                icon: FaGitAlt,
+                color: "green",
+                caption: "Git",
+                class: "iconClass"
+            }, {
+                icon: DiPostgresql,
+                color: "teal",
+                caption: "Postgres",
+                class: "iconClass"
+            }, {
+                icon: DiJavascript1,
+                color: "rgb(177, 160, 7)",
+                caption: "JavaScript",
+                class: "iconClass"
+            }, {
+                icon: TiHtml5,
+                color: "red",
+                caption: "HTML5",
+                class: "iconClass"
+            }
+        ]
+        const jsxIcons = iconArr.map((el) => {
+            return (
+                <div className="skills-icon-card">
+                    <el.icon style={{color: el.color}} className={el.class}/>
+                    <p>{el.caption}</p>
+                </div>
+            )
+        })
         return (
             <div className="skills-div">
-                <FaReact />
-                <FaNodeJs />
-                <DiPostgresql/>
-                <DiJavascript1/>
-                <TiHtml5/>
-                <FaGitAlt/>
+                {jsxIcons}
             </div>
         )
     }
