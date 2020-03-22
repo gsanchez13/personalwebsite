b74738dbf5518e6ffdc8754a74b59271b17d9439
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-22 14:25:41 -0400

    adds toggle component with some modifications to original styling

---
+++ client/src/Components/Toggle.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ToggleContainer from '../styling-files/toggled-styles';
+import { func, string } from 'prop-types';
+import { IoIosSunny, IoIosMoon } from 'react-icons/io';
+
+const Toggle = ({ theme, toggleTheme }) => {
+  const isLight = theme === 'light';
+  return (
+    <ToggleContainer onClick={toggleTheme}>
+      <IoIosSunny style={{color: "yellow"}}/>
+      <IoIosMoon style={{color: "yellow"}}/>
+    </ToggleContainer>
+  );
+};
+
+Toggle.propTypes = {
+  theme: string.isRequired,
+  toggleTheme: func.isRequired,
+}
+
+export default Toggle;
