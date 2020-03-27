5ef8e66ff5fe1fd1a52a0aecbba7d2d9a0217c42
gsanchez13 <gisellesanchez@pursuit.org>
2020-03-26 23:59:57 -0400

    adds objects to array for projects cards

--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 class Projects extends Component {
     render() {
         const projectThumbnails = [
-            require('../example1.jpeg'),
-            require('../example2.jpeg'),
-            require('../example3.jpeg'),
+            {
+                source: require('../example1.jpeg'),
+                codeLink: 'https://github.com/gsanchez13/ethicattire_hackathon'
+            },
+            {
+                source: require('../example2.jpeg'),
+                codeLink: 'https://github.com/gsanchez13/marathoner_movieapp'
+            },{
+                source: require('../example3.jpeg'),
+                codeLink: 'https://github.com/gsanchez13/ghibli_movieapp'
+            },
         ]
         const projectCard = (thumbnails) => {
 
