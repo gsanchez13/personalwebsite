810587a489fdb37c00e85261cd61b31651f08811
gsanchez13 <gisellesanchez@pursuit.org>
2020-06-21 10:32:22 -0400

    adds more to about me on personal portfolio

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -6,7 +6,8 @@ class About extends Component {
         return (
             <div className="about-div">
                 <div className="about-blurb">
-                    Born and raised in Brooklyn with a background in international business, I decided to change careers and get into coding. 
+                    Born and raised in Brooklyn with a background in International Business, I decided to change careers and get into coding. I was admitted into 
+                    <a href='https://www.pursuit.org/' className='pursuit-link'> Pursuit </a>where I learned programming languages like JavaScript, SQL, HTML and CSS. I enjoy playing soccer, cycling, reading and playing the ukulele when I'm not coding. As a full-stack web developer, I look forward to working on projects with energy, passion and an open mind! 
             </div>
                 <br />
                 <br />
