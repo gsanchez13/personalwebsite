0b4ca819064eeffd6146b9f7574c737502f1eab1
gsanchez13 <gisellesanchez@pursuit.org>
2020-06-21 17:03:06 -0400

    adds aboutClass to state hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Landing from './Components/Landing';
@@ -11,67 +11,64 @@ import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './styling-files/theme.js';
 import { GlobalStyles } from './styling-files/global.js';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      theme: 'light',
-      logo: require('./giselle.png'),
-      navName: 'light-nav',
-      footerName: 'light-footer',
-    }
-  }
-  changeTheme = () => {
-    if (this.state.theme === 'light') {
-      this.setState({
+const App = () => {
+  const [appState, setAppState] = useState({
+    theme: 'light',
+    logo: require('./giselle.png'),
+    navName: 'light-nav',
+    footerName: 'light-footer',
+    aboutBlurb: 'about-blurb-light'
+  });
+  const changeTheme = () => {
+    if (appState.theme === 'light') {
+      setAppState({
         theme: 'dark',
         logo: require('./giselledark.png'),
         navName: 'dark-nav',
         footerName: 'dark-footer',
-        iconColor: 'yellow'
+        iconColor: 'yellow',
+        aboutBlurb: 'about-blurb-dark'
       })
     }
     else {
-      this.setState({
+      setAppState({
         theme: 'light',
         logo: require('./giselle.png'),
         navName: 'light-nav',
         footerName: 'light-footer',
-        iconColor: 'grey'
+        iconColor: 'grey',
+        aboutBlurb: 'about-blurb-light'
       })
     }
   };
-  render() {
-
-    const { logo, theme, navName, footerName, iconColor} = this.state;
-    return (
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme} >
-        <div className="App" >
-          <GlobalStyles />
-            <Toggle theme={theme} toggleTheme={this.changeTheme} iconColor={iconColor}/>
-          <div className="logo-container">
-            <Link to="/">
-              <img src={logo} alt="logo" className="logo" />
-            </Link>
-          </div>
-          <div className="nav-container">
-            <NavBar class={navName} />
-          </div>
-          <div className="content-container">
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/projects" component={Projects} />
-              <Route exact path="/skills" component={Skills} />
-            </Switch>
-          </div>
-          <div className={'footer-container'}>
-            <Footer class={footerName} />
-          </div>
+  const { theme, logo, navName, footerName, iconColor, aboutBlurb} = appState;
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme} >
+      <div className="App" >
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={changeTheme} iconColor={iconColor} />
+        <div className="logo-container">
+          <Link to="/">
+            <img src={logo} alt="logo" className="logo" />
+          </Link>
         </div>
-      </ThemeProvider>
-    )
-  }
-}
+        <div className="nav-container">
+          <NavBar class={navName} />
+        </div>
+        <div className="content-container">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/about" render={() => (<About aboutClass={aboutBlurb}/>)}/>
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/skills" component={Skills} />
+          </Switch>
+        </div>
+        <div className={'footer-container'}>
+          <Footer class={footerName} />
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+};
 
 export default App;
