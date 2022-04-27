381c40f705872c9681d4e78544abf8485493f494
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-27 16:13:37 -0400

    test for resume in nav

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Pdf from './Resume.pdf';
+import Pdf from './resume.pdf';
 
 class NavBar extends Component {
     render() {
